baf61bbcbbab3842dd37175123381013b4e61814
Sawyer Wang <nordesertwolf@outlook.com>
2025-01-09 23:50:03 +0800

    学习完成 redis

--- Redis/Redis_Test1.py
+++ Redis/Redis_Test1.py
@@ -3,4 +3,8 @@ import redis
 #client = redis.StrictRedis()
 client2  = redis.StrictRedis(host='192.168.0.109', port=6379, db=0)
 
-client2.sadd('set2','https://www.baidu.com')
+#client2.sadd('URL_LIST','https://www.baidu.com')
+if client2.sadd('URL_LIST','https://www.baidu.com') == 1:
+    print('success')
+else:
+    print('fail')
