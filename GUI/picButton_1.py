649c33ef517b0733b7c82d337782993d47a83820
Sawyer Wang <41832156+VForVictoira@users.noreply.github.com>
2025-06-15 23:53:20 +0800

    Add files via upload

---
+++ GUI/picButton_1.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+def msgShow():
+    label.config(text='I love you',bg='lightyellow',fg='red')
+
+root = Tk()
+root.geometry('300x300')
+root.title('LOVE')
+label = Label(root)
+
+heart=PhotoImage(file='2.png')
+btn = Button(root,image=heart,
+             text='I love you',command=msgShow
+             )
+label.pack()
+btn.pack(side=BOTTOM,anchor=S)
+root.mainloop()
