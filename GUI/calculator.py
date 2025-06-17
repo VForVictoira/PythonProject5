80816d9ff99aa5fc1f03a5f9fd3f7785154845c4
SawyerWang <nordesertwolf@outlook.com>
2025-06-17 14:43:53 +0800

    完成计算器的编写

--- GUI/calculator.py
+++ GUI/calculator.py
@@ -1,44 +1,51 @@
 from tkinter import *
 
+def calculate():
+    result = eval(equ.get())
+    equ.set(equ.get() + '=\n' + str(result))
+
+def show(buttonString):
+    content = equ.get()
+    if content == '0':
+        content = ''
+    equ.set(content+buttonString)
+
+def backspace():
+    equ.set(str(equ.get()[:-1]))
+
+def clear():
+    equ.set('0')
+
 window = Tk()
 window.title("Calculator")
+for i in range(5):
+    window.rowconfigure(i, weight=1)
+for c in range(3):
+    window.columnconfigure(c, weight=1)
+equ = StringVar()
+equ.set('0')
 
-
-
-label = Label(window, text="Enter your number:",bg='gray',width=20)
-btn7 = Button(text='7',width=3)
-btn8 = Button(text='8',width=3)
-btn9 = Button(text='9',width=3)
-btn0 = Button(text='0',width=10)
-btn1 = Button(text='1',width=3)
-btn2 = Button(text='2',width=3)
-btn3 = Button(text='3',width=3)
-btn4 = Button(text='4',width=3)
-btn5 = Button(text='5',width=3)
-btn6 = Button(text='6',width=3)
-btnM = Button(text='*',width=3)
-btnS = Button(text='+',width=3)
-btnD = Button(text='-',width=3)
-btnL = Button(text='/',width=3)
-btnDt = Button(text='.',width=3)
-btnE = Button(text='=',width=3)
-
-label.grid(row=0,column=0,columnspan=4)
-btn7.grid(row=1,column=0,padx=5)
-btn8.grid(row=1,column=1,padx=5)
-btn9.grid(row=1,column=2,padx=5)
-btnS.grid(row=1,column=3,padx=5)
-btn4.grid(row=2,column=0,padx=5)
-btn5.grid(row=2,column=1,padx=5)
-btn6.grid(row=2,column=2,padx=5)
-btnD.grid(row=2,column=3,padx=5)
-btn1.grid(row=3,column=0,padx=5)
-btn2.grid(row=3,column=1,padx=5)
-btn3.grid(row=3,column=2,padx=5)
-btnM.grid(row=3,column=3,padx=5)
-btn0.grid(row=4,column=0,columnspan=2,padx=5)
-btnDt.grid(row=4,column=2,padx=5)
-btnL.grid(row=4,column=3,padx=5)
-btnE.grid(row=5,column=3,padx=5)
-
+# 设计显示区
+Label(window, textvariable=equ,bg='gray',width=20).grid(row=0,column=0,columnspan=4)
+Button(window,fg='blue',command=clear,text='C',width=3).grid(row=1,column=0,padx=5)
+#清除显示区按钮
+Button(window,text='DEL',width=3,command=backspace).grid(row=1,column=1,padx=5)
+#其他按钮
+btnpe = Button(window,text='%',width=3,command=lambda:show('%')).grid(row=1,column=2,padx=5)
+btnL = Button(window,text='/',width=3,command=lambda:show('/')).grid(row=1,column=3,padx=5)
+btn7 = Button(window,text='7',width=3,command=lambda:show('7')).grid(row=2,column=0,padx=5)
+btn8 = Button(window,text='8',width=3,command=lambda:show('8')).grid(row=2,column=1,padx=5)
+btn9 = Button(window,text='9',width=3,command=lambda:show('9')).grid(row=2,column=2,padx=5)
+btnS = Button(window,text='+',width=3,command=lambda:show('+')).grid(row=2,column=3,padx=5)
+btn4 = Button(window,text='4',width=3,command=lambda:show('4')).grid(row=3,column=0,padx=5)
+btn5 = Button(window,text='5',width=3,command=lambda:show('5')).grid(row=3,column=1,padx=5)
+btn6 = Button(window,text='6',width=3,command=lambda:show('6')).grid(row=3,column=2,padx=5)
+btnD = Button(window,text='-',width=3,command=lambda:show('-')).grid(row=3,column=3,padx=5)
+btn1 = Button(window,text='1',width=3,command=lambda:show('1')).grid(row=4,column=0,padx=5)
+btn2 = Button(window,text='2',width=3,command=lambda:show('2')).grid(row=4,column=1,padx=5)
+btn3 = Button(window,text='3',width=3,command=lambda:show('3')).grid(row=4,column=2,padx=5)
+btnM = Button(window,text='*',width=3,command=lambda:show('*')).grid(row=4,column=3,padx=5)
+btn0 = Button(window,text='0',width=10,command=lambda:show('0')).grid(row=5,column=0,columnspan=2,padx=5)
+btnDt = Button(window,text='.',width=3,command=lambda:show('.')).grid(row=5,column=2,padx=5)
+btnE = Button(window,text='=',width=3,command=calculate,bg='yellow').grid(row=5,column=3,padx=5)
 window.mainloop()
